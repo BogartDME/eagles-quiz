fbd17a7a419eb229eecd5a80253fa0e8f93ff4f0
BogartDME <edwin.pietrowski@gmail.com>
2023-01-03 14:53:15 -0500

    added functions for endquiz, display scoreboard, questions, grading questions. Also refactored questions.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -6,15 +6,16 @@ var scores = document.querySelector("#hs-container");
 var timeEl = document.querySelector("#clock");
 var startGame = document.querySelector("#start");
 var gameOver = document.querySelector(".all-done")
-var timeLeft = 59;
 var timerInterval;
 var penalty;
-
-//hide all the elements that need to be hidden until needed
-myQuestions.style.display = "none"; 
-scores.style.display = "none"; 
-gameOver.style.display = "none";
-
+var timeLeft; 
+let currentQuestion;  
+var question = document.getElementById("question");
+var choice1 = document.getElementById("A"); 
+var choice2 = document.getElementById("B"); 
+var choice3 = document.getElementById("C"); 
+var choice4 = document.getElementById("D"); 
+var initiateQuiz = myQuestions.length-1; 
 
 //create function for a timer
 function startTimer() {
@@ -23,85 +24,120 @@ function startTimer() {
         timeEl.textContent = timeLeft;
         if(timeLeft <= 0) { 
         clearInterval(timerInterval);
-        gameOver();
+        window.alert("Oops, you don't know much about the Eagles!");
     }
     }, 1000);
     
 }
-// create function to show questions
-var displayQuestions = function(){
-    document.getElementById("start").style.display = "none;"
-    document.getElementById("quiz-container").style.display = "block";
-    }
-    //Create event listener to activate start game expression
-    document.getElementById("start").addEventListener("click", startGame);
-    console.log("start")
     
-    //create start game expression that intiates timer and question functions
-    var startGame = function (){ displayQuestions, startTimer
-        console.log("game has started")
+function questions(){
+    let nextQuestion = myQuestions[currentQuestion];
+        question.innerHTML = nextQuestion.question;
+        choice1.innerHTML = nextQuestion.choice1;
+        choice2.innerHTML = nextQuestion.choice2;
+        choice3.innerHTML = nextQuestion.choice3;
+        choice4.innerHTML = nextQuestion.choice4;
+}
+
+function gradingQuestions(gradeQuestion) {
+    penalty = timeLeft -5;
+    if (gradeQuestion == myQuestions[currentQuestion].correct){
+        currentQuestion++
+        if (initiateQuiz >= currentQuestion) {
+            questions();
+    }}
+    else if (gradeQuestion != myQuestions[currentQuestion].correct){
+        timeLeft = penalty;
+        currentQuestion++
+    } if (initiateQuiz >= currentQuestion) {
+            questions();
+    } if (currentQuestion == myQuestions.length){
+        return endQuiz();
     }
-    
+};
+
     
 //create questions list
 var myQuestions = [
-    {
-        question: "What is Brian Dawkins' number?",
-        choices: [
+    
+        
             { 
+                question: "What is Brian Dawkins' number?",
                 choice1:"7", 
                 choice2:"20",
                 choice3:"24",
-                correct: "20", //or false
+                choice4:"30",
+                correct: "B", //or false
 
-            }
-        ]
+            },
     
-    },
-    
-    {
-        question: "What was Brian Dawkins' number?",
-        choices: [
-            { 
-                choice1:"33", 
-                choice2:"20",
-                choice3:"24",
-                choice4:"32",
-                correct: "20" //or false
+            {
+            
+            
+                question: "How many Superbowl appearances have the Eagles had?",
+                choice1:"4", 
+                choice2:"1",
+                choice3:"2",
+                choice4:"3",
+                correct: "D" //or false
 
-            }
-        ]
-    
-    },
-    {
-        question: "How many Super Bowl appearances have the Eagles had?",
-        choices: [
-            { 
-                choice1:"3", 
-                choice2:"2",
-                choice3:"7",
-                choice:"5",
-                correct: "3" //or false
+            },
 
-            }
-        ]
+            {
+            
+                question: "Which quarterback has tied the league record for most touchdown passes in one game?",
+                choice1:"Donavan McNabb", 
+                choice2:"Ron Jawarsky",
+                choice3:"Nick Foles",
+                choice:"Randall Cunningham",
+                correct: "C" //or false
+
+            },
     
-    },
-    {
-        question: "Which comic book characters have been tied to the Eagles in the last 15 years?",
-        choices: [
-            { 
+            {
+        
+                question: "Which comic book characters have been tied to the Eagles in the last 15 years?",
                 choice1:"Batman", 
+                choice4: "Batman and Wolverine",
                 choice2:"Punisher",
                 choice3:"Wolverine",
-                choice4: "Batman and Wolverine",
-                correct: "Batman and Wolverine" //or false
+                correct: "B" //or false
 
             }
-        ]
-    
-    }
 ];
+
+let topScores = JSON.parse(localStorage.getItem("player&score")) || [];
+
+var endQuiz = function(){
+    var playerInput = window.prompt("Good job! Please enter your name to be added to the scoreboard!");
+    playerName = playerInput;
+    clearInterval(timerInterval);
+    playerInfo = {
+        name: playerName.trim,
+        timer: timeLeft
+    };
+    score.push(playerInfo);
+    localStorage.setItem("player&score", JSON.stringify(score));
+    displayScoreBoard();
+}
+
+function displayScoreBoard(){
+    let string = ""
+    for( let i = 0; i<score.length; i++){
+        string += `<li> ${score[i].name} ${score[i].timer}<li>`
+    }
+    document.querySelector("#hs-header").innerHTML = string
+};
+
+startGame.addEventListener("click", function(){
+    timeLeft= 60;
+    myQuestions = 0;
+    startTimer();
+    questions();
+});
+
+
+
 
 
 
