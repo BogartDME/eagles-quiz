e6bc59e6d0f2e53f5bab1312131a3575adb38238
BogartDME <edwin.pietrowski@gmail.com>
2023-01-05 14:17:10 -0500

    quiz is now  initiating

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -87,7 +87,7 @@ function quizQuestions(){
 }
 startGame.addEventListener("click", function(){
     timeLeft= 60;
-    myQuestions = 0;
+    currentQuestion = 0;
     startTimer();
     quizQuestions();
 });
@@ -141,31 +141,31 @@ function displayScoreBoard(){
 
 
 
-// Get the modal
-// Get the button that opens the modal
-var btn = document.getElementById("modalBtn");
+// // Get the modal
+// // Get the button that opens the modal
+// var btn = document.getElementById("modalBtn");
 
-var modal = document.getElementById("instructions-modal");
+// var modal = document.getElementById("instructions-modal");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
 
 
 
