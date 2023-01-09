afdd07c80567ed998238efa9c03b2e2f851b1aa2
BogartDME <edwin.pietrowski@gmail.com>
2023-01-09 12:08:17 -0500

    debugged JS and got the quiz to cycle through questions

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -97,20 +97,26 @@ function gradingQuestions(gradeQuestion) {
     if (gradeQuestion == myQuestions[currentQuestion].correct){
         currentQuestion++
         if (initiateQuiz >= currentQuestion) {
-            questions();
+            quizQuestions();
     }}
     else if (gradeQuestion != myQuestions[currentQuestion].correct){
         timeLeft = penalty;
         currentQuestion++
     } if (initiateQuiz >= currentQuestion) {
-            questions();
+            quizQuestions();
     } if (currentQuestion == myQuestions.length){
         return endQuiz();
     }
 };
 
     
-//create questions list
+
+
+
+
+
+
+//Display Scoreboard
 
 let topScores = JSON.parse(localStorage.getItem("player&score")) || [];
 
@@ -119,58 +125,53 @@ var endQuiz = function(){
     playerName = playerInput;
     clearInterval(timerInterval);
     playerInfo = {
-        name: playerName.trim,
+        name: playerName.trim(),
         timer: timeLeft
     };
-    score.push(playerInfo);
-    localStorage.setItem("player&score", JSON.stringify(score));
+    topScores.push(playerInfo);
+    localStorage.setItem("player&score", JSON.stringify(topScores));
+    console.log(topScores);
     displayScoreBoard();
 }
 
 function displayScoreBoard(){
     let string = ""
-    for( let i = 0; i<score.length; i++){
-        string += `<li> ${score[i].name} ${score[i].timer}<li>`
+    for( let i = 0; i<topScores.length; i++){
+        string += `<li> ${topScores[i].name} ${topScores[i].timer}<li>`
     }
-    document.querySelector("#hs-header").innerHTML = string
+    document.querySelector(".display-scores").innerHTML = string
 };
 
 
 
 
 
-
-
 // // Get the modal
-// // Get the button that opens the modal
-// var btn = document.getElementById("modalBtn");
+// Get the button that opens the modal
+var btn = document.getElementById("modalBtn");
 
-// var modal = document.getElementById("instructions-modal");
+var modal = document.getElementById("instructions-modal");
 
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-// // When the user clicks on the button, open the modal
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
 
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
 
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
-
-
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 
-//scoreboard functions
-//create gameover function    
-// var gameOver = function
-//create document highscore function
+
+
